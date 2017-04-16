2aba7e6b024ec02218b19168b5751972b55d1a20
dwalsten5 <Chelonia1>
2017-04-15 21:53:17 -0400

    True initial commit

---
+++ src/main/java/TCBigQueryClient.java
@@ -0,0 +1,12 @@
+package main.java;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TCBigQueryClient {
+	
+	
+	public static void main(String[] args) throws IOException, FileNotFoundException {
+		
+	}
+}
