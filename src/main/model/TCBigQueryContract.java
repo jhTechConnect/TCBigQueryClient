3700e1789924cb625a69e9edeb4f70941fe16738
dwalsten5 <Chelonia1>
2017-06-13 09:38:27 -0400

    Remove obsolete queries

--- src/main/model/TCBigQueryContract.java
+++ src/main/model/TCBigQueryContract.java
@@ -4,53 +4,6 @@ public class TCBigQueryContract {
 	
 	public static class SessionDataEntry {
 	
-		//Will need to modify based on the data window that we're interested
-		//This is across all users, not for an individual user
-		public static final String COMPLETE_QUERY = "SELECT User,date(thing1) as Day,ID,Name " +
-        		"FROM FLATTEN(FLATTEN(SELECT user_dim.app_info.app_instance_id as User,event_dim.timestamp_micros as thing1, event_dim.params.value.string_value as ID, " +
-        		"FROM " + 
-        		  "TABLE_DATE_RANGE(org_techconnect_ANDROID.app_events_, TIMESTAMP('2017-03-01'), TIMESTAMP('2017-04-07')) " +
-        		  "WHERE event_dim.name = 'session_complete' AND event_dim.params.key = 'item_id', event_dim.params.value),event_dim) t1, " +
-        		  "LEFT JOIN " +
-        		  "FLATTEN(FLATTEN(SELECT event_dim.timestamp_micros as thing2,event_dim.params.value.string_value as Name, " +
-        		  "FROM " +
-        		  "TABLE_DATE_RANGE(org_techconnect_ANDROID.app_events_, TIMESTAMP('2017-03-01'), TIMESTAMP('2017-04-07')) " +
-        		  "WHERE event_dim.name = 'session_complete' AND event_dim.params.key = 'item_name', event_dim.params.value),event_dim) t2 " +
-        		  "On t1.thing1 = t2.thing2 " +
-        		  "ORDER BY thing1;";
-		
-		public static final String COMPLETE_QUERY_INDIVIDUAL = "SELECT User,date(thing1) as Day,ID,Name " +
-        		"FROM FLATTEN(FLATTEN(SELECT user_dim.app_info.app_instance_id as User,event_dim.timestamp_micros as thing1, event_dim.params.value.string_value as ID, " +
-        		"FROM " + 
-        		  "TABLE_DATE_RANGE(org_techconnect_ANDROID.app_events_, TIMESTAMP('2017-03-01'), TIMESTAMP('2017-04-07')) " +
-        		  "WHERE user_dim.app_info.app_instance_id = '%s' AND event_dim.name = 'session_complete' AND event_dim.params.key = 'item_id', event_dim.params.value),event_dim) t1, " +
-        		  "LEFT JOIN " +
-        		  "FLATTEN(FLATTEN(SELECT event_dim.timestamp_micros as thing2,event_dim.params.value.string_value as Name, " +
-        		  "FROM " +
-        		  "TABLE_DATE_RANGE(org_techconnect_ANDROID.app_events_, TIMESTAMP('2017-03-01'), TIMESTAMP('2017-04-07')) " +
-        		  "WHERE user_dim.app_info.app_instance_id = '%s' AND event_dim.name = 'session_complete' AND event_dim.params.key = 'item_name', event_dim.params.value),event_dim) t2 " +
-        		  "On t1.thing1 = t2.thing2 " +
-        		  "ORDER BY thing1;";
-		
-		public static final String ENDEARLY_QUERY_INDIVIDUAL = "SELECT date(thing1) as Day,ID,Name " +
-			"FROM FLATTEN(FLATTEN(SELECT event_dim.timestamp_micros as thing1, event_dim.params.value.string_value as ID, " +
-			"FROM " +
-			"TABLE_DATE_RANGE(org_techconnect_ANDROID.app_events_, TIMESTAMP('%s'), TIMESTAMP('%s')) " +
-			"WHERE user_dim.app_info.app_instance_id = '%s' AND event_dim.name = 'session_end_early' AND event_dim.params.key = 'item_id', event_dim.params.value),event_dim) t1, " +
-			"LEFT JOIN " +
-			"FLATTEN(FLATTEN(SELECT event_dim.timestamp_micros as thing2,event_dim.params.value.string_value as Name, " +
-			"FROM " + 
-			"TABLE_DATE_RANGE(org_techconnect_ANDROID.app_events_, TIMESTAMP('%s'), TIMESTAMP('%s')) " +
-			"WHERE user_dim.app_info.app_instance_id = '%s' AND event_dim.name = 'session_end_early' AND event_dim.params.key = 'item_name', event_dim.params.value),event_dim) t2 " +
-			"On t1.thing1 = t2.thing2 " +
-			"ORDER BY thing1";
-		
-		public static final String COMPLETE_DAILY_COUNT = "SELECT date(event_dim.timestamp_micros) as day, count(*) as Count, " +
-			"FROM " +
-			"TABLE_DATE_RANGE(org_techconnect_ANDROID.app_events_, TIMESTAMP('2017-03-01'), TIMESTAMP('2017-04-07')) " +
-			"WHERE event_dim.name = 'session_complete' AND event_dim.params.key = 'item_id' " +
-			"GROUP BY day " +
-			"ORDER BY day";	
 		
 		public static final String ALL_EVENTS_DAILY_COUNT_INDIVIDUAL = "SELECT Day, COALESCE(INTEGER(count7),0) as BeginCount, PausedCount, DeleteCount, ResumeCount, EarlyCount, CompleteCount "+
 				"FROM (SELECT Day, COALESCE(INTEGER(count6),0) as PausedCount, DeleteCount,ResumeCount,EarlyCount,CompleteCount " +
@@ -176,12 +129,6 @@ public class TCBigQueryContract {
 		
 	}
 	public static class GeneralInfoEntry {
-		
-		public static final String ALL_USER_APP_SESSIONS = "SELECT event_dim.name as Event, Date(event_dim.timestamp_micros) as Day,event_dim.timestamp_micros as DayMicros " + 
-		  "FROM " + 
-		  "TABLE_DATE_RANGE(org_techconnect_ANDROID.app_events_, TIMESTAMP('%s'), TIMESTAMP('%s')) " +
-		  "WHERE user_dim.app_info.app_instance_id = '%s' AND event_dim.name = 'session_start' " +
-		  "ORDER BY DayMicros";
 		
 		public static final String ALL_USER_INFO_SESSIONS = "SELECT Event, date(thing1) as Date, thing1 as Raw, Class, EngagementTime FROM " +
 				  "FLATTEN(FLATTEN(SELECT event_dim.name as Event, event_dim.timestamp_micros as thing1, event_dim.params.value.string_value as Class, " +
